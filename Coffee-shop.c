62884ca340547281577185c6ce0e05b3e23967be
fariya056 <149135569+fariya056@users.noreply.github.com>
2023-11-03 22:28:21 +0600

    Update Coffee-shop.c
    Main

---
+++ Coffee-shop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<dos.h>
+#include<conio.h>
+#include"SplashScreen.h"
+#include"Login"
+int main()
+{
+int choose;
+SplashScreen();
+sleep(5);
+system("CLS");
+return 0;
+}
