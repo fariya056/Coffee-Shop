e7d2f678d222f45e0b8626117dd65807752effab
fariya056 <149135569+fariya056@users.noreply.github.com>
2023-11-04 19:39:59 +0600

    Update Login.h

---
+++ Login.h
@@ -0,0 +1,53 @@
+registration
+    struct login
+    {
+       char Name[100] ;
+       char Username[20];
+       char Password[20];
+    };
+    regi()
+    {
+        FILE *log;
+        log=fopen("Login.txt","w");
+        struct login l;
+        printf("Enter Name: ");
+        scanf("%s",l.Name);
+        printf("Enter User Name: ");
+        scanf("%s",l.Username);
+        printf("Enter pass word: ");
+        scanf("%s",l.Password);
+        //adding the entered data in a txt file
+        fwrite(&l,sizeof(l),1,log);
+        fclose(log);
+        printf("Registration compleat");
+        printf("\n Press any key to continue");
+        getch();
+        system("CLS");
+        login();
+    }
+    login()
+    {
+        char username[20];
+        char password[20];
+        FILE *log;
+        log=fopen("Login.txt","r");
+        struct login l;
+        printf("Enter User Name: ");
+        scanf("%s",&username);
+        printf("Enter Password: ");
+        scanf("%s",&password);
+        //running loop to compare the user name and password
+        while(fread(&l,sizeof(l),1,log))
+        {
+            if(strcmp(username,l.Username)==0&&strcmp(password,l.Password)==0)
+            {
+                printf("Login Successful");
+            }
+            else
+            {
+                printf("Wrong User Name or Password");
+            }
+        }
+        fclose(log);
+
+    }
