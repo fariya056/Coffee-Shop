4b43f60cfa39d7b65a4d95c2ffbd0bb9efbdd41b
fariya056 <149135569+fariya056@users.noreply.github.com>
2023-11-13 10:39:14 +0600

    Update Coffee-shop.c

--- Coffee-shop.c
+++ Coffee-shop.c
@@ -9,5 +9,16 @@ int choose;
 SplashScreen();
 sleep(5);
 system("CLS");
+printf("Press 1 to register\nPress 2 to login");
+scanf("%d",&c);
+if(choose==1)
+{
+    system("CLS");
+    regi(); 
+}
+else if(choose==0)
+{
+    system(CLS);
+    login();
 return 0;
 }
